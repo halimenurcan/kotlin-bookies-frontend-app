77aba5f981c2b199c6c5ec1fc32a0d485e90dd91
sametavci <sametavci03@gmail.com>
2025-05-15 13:45:37 +0200

    Der neue Method wurde implementiert

--- app/src/main/java/com/example/frontendbook/data/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/com/example/frontendbook/data/repository/AuthRepositoryImpl.kt
@@ -1,8 +1,10 @@
 package com.example.frontendbook.data.repository
 import com.example.frontendbook.data.model.register.RegisterRequest
 import com.example.frontendbook.data.model.register.RegisterResponse
+import com.example.frontendbook.data.model.resetPassword.ResetPasswordResponse
 import com.example.frontendbook.data.model.signIn.SignInRequest
 import com.example.frontendbook.data.model.signIn.SignInResponse
+import com.example.frontendbook.domain.model.ResetPasswordParams
 import com.example.frontendbook.domain.repository.AuthRepository
 import com.example.frontendbook.retrofit.ApiService
 import retrofit2.Response
@@ -17,5 +19,9 @@ class AuthRepositoryImpl @Inject constructor(
 
     override suspend fun register(request: RegisterRequest): Response<RegisterResponse> {
         return apiService.register(request)
+    }
+
+    override suspend fun resetPassword(request: ResetPasswordParams): Response<ResetPasswordResponse> {
+        TODO("Not yet implemented")
     }
 }
