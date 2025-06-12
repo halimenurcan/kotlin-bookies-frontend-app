c29b3b2f8538d37254003c743194bad694f7bc74
Çağlı, Ayberk <ayberk.cagli@study.thws.de>
2025-06-13 01:22:30 +0200

    bottom_sheet_choose_profile_image.xml und ChooseProfileImageBottomSheetFragment.kt wurde erstellt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,8 @@ dependencies {
 
     // ✅ Glide
     implementation("com.github.bumptech.glide:glide:4.15.1")
+    implementation(libs.androidx.legacy.support.v4)
+    implementation(libs.androidx.recyclerview)
     kapt("com.github.bumptech.glide:compiler:4.15.1")
 
     // ✅ Navigation
